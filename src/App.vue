d5dd7682868d6c0c0299c25110f920c0b072a895
krelbaum <ae.krelbaum@gmail.com>
2023-11-20 20:05:43 +0100

    Added red text

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
 <template>
-  <div>
-    <textarea v-model="jsonInput" placeholder="Enter JSON here"></textarea>
-    <button @click="formatJSON">Format</button>
-    <pre>{{ formattedJSON }}</pre>
+  <div class="json-formatter">
+    <h2>JSON Formatter</h2>
+    <textarea
+        v-model="jsonInput"
+        placeholder="Enter JSON here"
+        class="json-input"
+    ></textarea>
+    <button @click="formatJSON" class="format-button">Format</button>
+    <div class="formatted-json">
+      <pre v-if="formattedJSON">{{ formattedJSON }}</pre>
+      <p v-else class="error-message">{{ errorMessage }}</p>
+    </div>
   </div>
 </template>
 
@@ -11,7 +19,8 @@ export default {
   data() {
     return {
       jsonInput: '',
-      formattedJSON: ''
+      formattedJSON: '',
+      errorMessage: ''
     };
   },
   methods: {
@@ -19,21 +28,34 @@ export default {
       try {
         const parsedJSON = JSON.parse(this.jsonInput);
         this.formattedJSON = JSON.stringify(parsedJSON, null, 2);
+        this.errorMessage = '';
       } catch (error) {
-        this.formattedJSON = 'Invalid JSON format';
+        this.errorMessage = 'Invalid JSON format';
+        this.formattedJSON = '';
       }
     }
   }
 };
 </script>
 
-<style>
-textarea {
+<style scoped>
+.json-formatter {
+  max-width: 600px;
+  margin: 0 auto;
+  font-family: Arial, sans-serif;
+}
+
+.json-input {
   width: 100%;
   height: 150px;
-  margin-bottom: 10px;
+  padding: 10px;
+  margin-bottom: 15px;
+  border: 1px solid lightgrey;
+  border-radius: 4px;
+  resize: vertical;
 }
-button {
+
+.format-button {
   padding: 8px 16px;
   background-color: blue;
   color: white;
@@ -41,10 +63,17 @@ button {
   border-radius: 4px;
   cursor: pointer;
 }
-pre {
-  background-color: lightgrey;
+
+.formatted-json pre {
+  background-color: white;
   padding: 10px;
   border-radius: 4px;
   white-space: pre-wrap;
+}
+
+.error-message {
+  color: red;
+  font-weight: bold;
+  margin-top: 5px;
 }
 </style>
