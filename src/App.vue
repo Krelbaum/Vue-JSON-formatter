1e59a04d5893588e540b27e3c3af83f220e9aee4
krelbaum <ae.krelbaum@gmail.com>
2023-11-20 20:10:45 +0100

    Added buttons 'short' - 'long'

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,13 @@
         placeholder="Enter JSON here"
         class="json-input"
     ></textarea>
-    <button @click="formatJSON" class="format-button">Format</button>
+    <div class="button-container">
+      <button @click="formatJSON" class="format-button">Format</button>
+      <div class="toggle-buttons">
+        <button @click="toggleFormat('short')" :class="{ active: format === 'short' }">Short</button>
+        <button @click="toggleFormat('long')" :class="{ active: format === 'long' }">Long</button>
+      </div>
+    </div>
     <div class="formatted-json">
       <pre v-if="formattedJSON">{{ formattedJSON }}</pre>
       <p v-else class="error-message">{{ errorMessage }}</p>
@@ -20,18 +26,27 @@ export default {
     return {
       jsonInput: '',
       formattedJSON: '',
-      errorMessage: ''
+      errorMessage: '',
+      format: 'short' // format po defoltu
     };
   },
   methods: {
     formatJSON() {
       try {
         const parsedJSON = JSON.parse(this.jsonInput);
-        this.formattedJSON = JSON.stringify(parsedJSON, null, 2);
+        this.formattedJSON = this.format === 'long'
+            ? JSON.stringify(parsedJSON, null, 4)
+            : JSON.stringify(parsedJSON);
         this.errorMessage = '';
       } catch (error) {
         this.errorMessage = 'Invalid JSON format';
         this.formattedJSON = '';
+      }
+    },
+    toggleFormat(type) {
+      this.format = type;
+      if (this.formattedJSON) {
+        this.formatJSON();
       }
     }
   }
@@ -44,7 +59,6 @@ export default {
   margin: 0 auto;
   font-family: Arial, sans-serif;
 }
-
 .json-input {
   width: 100%;
   height: 150px;
@@ -54,7 +68,6 @@ export default {
   border-radius: 4px;
   resize: vertical;
 }
-
 .format-button {
   padding: 8px 16px;
   background-color: blue;
@@ -63,17 +76,35 @@ export default {
   border-radius: 4px;
   cursor: pointer;
 }
-
 .formatted-json pre {
   background-color: white;
   padding: 10px;
   border-radius: 4px;
   white-space: pre-wrap;
 }
-
 .error-message {
   color: red;
   font-weight: bold;
   margin-top: 5px;
+}
+
+
+.toggle-buttons {
+  margin-top: 10px;
+}
+
+.toggle-buttons button {
+  padding: 5px 10px;
+  margin-right: 10px;
+  border: 1px solid #007bff;
+  background-color: transparent;
+  color: #007bff;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+.toggle-buttons button.active {
+  background-color: #007bff;
+  color: white;
 }
 </style>
